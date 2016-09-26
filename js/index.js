fe928cb805973c14b0ef87764c7be3f634383e47
Marc Shapiro <marcnshap@gmail.com>
2016-09-25 22:48:34 -0700

    Add tomato image behind output.  Add ding at end of each pomodoro and break.  Enable adjusting length of pomodoros and breaks.

--- js/index.js
+++ js/index.js
@@ -8,6 +8,8 @@ var hrs = 0;
 var mins = 0;
 var secs = 0;
 
+var isBreak = false;
+
 var countdown = function() {
   if (running) {
     if (secs < 0) {
@@ -23,11 +25,38 @@ var countdown = function() {
     var strMinutes = (mins < 10 ? "0" + mins: mins.toString());
     var strSeconds = (secs < 10 ? "0" + secs: secs.toString());
 
-    var remainingTime = "Time remaining: " + strHours + ":" + strMinutes + ":" + strSeconds;
+    var remainingTime = "";
 
-    document.getElementById("output").innerHTML = remainingTime;
+    if (isBreak) {
+      remainingTime = "Break remaining: " + strMinutes + ":" + strSeconds;
+      document.getElementById("output").innerHTML = remainingTime;
+    } else {
+      remainingTime = "Time remaining: " + strHours + ":" + strMinutes + ":" + strSeconds;
+      document.getElementById("output").innerHTML = remainingTime;
+    }
 
     secs--;
+
+    if (hrs + mins + secs === -1) {
+      isBreak = !isBreak;
+
+      if (isBreak) {
+          hrs = 0;
+          mins = lenBreak;
+          secs = 0;
+      } else {
+        if (lenSession > 59) {
+          hrs = Math.trunc(lenSession / 60);
+          mins = lenSession % 60;
+          secs = 0;
+        } else {
+          hrs = 0;
+          mins = lenSession;
+          secs = 0;
+        }
+      }
+      new Audio("resources/ding.mp3").play()
+    }
 
     setTimeout(countdown, 1000);
   }
@@ -49,6 +78,7 @@ $(document).ready( function() {
   document.getElementById("sessionTime").innerHTML = lenSession;
   document.getElementById("breakTime").innerHTML = lenBreak;
 
+  $("#output")
   $("#start").on("click", function() {
     if (!running) {
       running = true;
@@ -82,5 +112,32 @@ $(document).ready( function() {
 
     document.getElementById("output").innerHTML = remainingTime;
   });    
+
+  $("#decSession").on("click", function() {
+    if (lenSession > 1) {
+        lenSession--;
+        document.getElementById("sessionTime").innerHTML = lenSession;    
+    }
+  });    
+  $("#incSession").on("click", function() {
+    if (lenSession < 60) {
+        lenSession++;
+        document.getElementById("sessionTime").innerHTML = lenSession;    
+    }
+  });    
+  $("#decBreak-").on("click", function() {
+    if (lenBreak > 1) {
+        lenBreak--;
+        document.getElementById("breakTime").innerHTML = lenBreak;    
+    }
+  });    
+  $("#incBreak").on("click", function() {
+    if (lenBreak < 10) {
+        lenBreak++;
+        document.getElementById("breakTime").innerHTML = lenBreak;    
+    }
+  });    
+
+  
 });
 
